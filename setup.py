67b2df41333c79d5412c713a3fe2e77d9e37587c
Anna Medyukhina <37274810+amedyukhina@users.noreply.github.com>
2022-03-17 10:47:38 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='am_utils',
-    version='0.1',
+    version='0.2',
     url="https://github.com/amedyukhina/am_utils",
     author="Anna Medyukhina",
     author_email='anna.medyukhina@gmail.com',
